f0927491facee6741f7e17fcaae6d6908bb5bb7e
nadirov-etibar <nadirov.etibar@gmail.com>
2019-11-16 23:51:20 +0400

    Initial commit

--- JavaScript/HomeWork_9/js/main.js
+++ JavaScript/HomeWork_9/js/main.js
@@ -1,13 +1,32 @@
+// let tabs = document.getElementsByClassName("tabs-title");
+// let content = document.getElementsByClassName("content");
+//
+// for (let i = 0; i < tabs.length; i++) {
+//     tabs[i].addEventListener("click", function() {
+//         let active = document.getElementsByClassName("active");
+//         active[0].className = active[0].className.replace(" active", "");
+//         tabs[i].className += " active";
+//         let contentActive = document.getElementsByClassName("content-active");
+//         contentActive[0].className = contentActive[0].className.replace(" content-active", "");
+//         content[i].className += " content-active";
+//     });
+// }
+
+// WITH DATASET 
+
 let tabs = document.getElementsByClassName("tabs-title");
 let content = document.getElementsByClassName("content");
 
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", function() {
-        let active = document.getElementsByClassName("active");
-        active[0].className = active[0].className.replace(" active", "");
-        tabs[i].className += " active";
-        let contentActive = document.getElementsByClassName("content-active");
-        contentActive[0].className = contentActive[0].className.replace(" content-active", "");
-        content[i].className += " content-active";
-    });
+for (let i = 0; i < tabs.length; i++){
+    tabs[i].addEventListener("click", function () {
+        let idNumber = tabs[i].dataset.id;
+        let contentId = document.getElementById("active_tab_"+idNumber);
+        for (let j = 0; j < content.length; j++){
+            tabs[j].classList.remove("active");
+            content[j].classList.remove("content-active");
+        }
+        tabs[i].classList.add("active");
+        contentId.classList.add("content-active");
+    })
 }
+
